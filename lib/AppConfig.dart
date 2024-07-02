b84640454b4cad652a96418878491971069b60ae
jansugint <jan@exopek.de>
2024-07-02 17:20:33 +0200

    chore: new backend url

--- lib/AppConfig.dart
+++ lib/AppConfig.dart
@@ -1,6 +1,6 @@
 abstract class AppConfig {
   static const String apiBaseUrl =
-      "http://exopek.azurewebsites.net/api/"; /* String.fromEnvironment(
+      "https://api.exopek.de/api/";/* "http://exopek.azurewebsites.net/api/"; */ /* String.fromEnvironment(
     'API_BASE_URL',
     defaultValue: '',
   ); */
